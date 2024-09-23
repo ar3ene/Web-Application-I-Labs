0c51cd463583602036ef6c2ee6a45b6535caeaac
yk <yk@ykdeMacBook-Pro.local>
2024-09-23 22:31:59 +0800

    update: lab1 solution

--- lab1.js
+++ lab1.js
@@ -33,6 +33,32 @@ function firstTwoLastTwo(str) {
 // Then, implement the addNewFilm method, which adds a new Film object, passed as parameter, to the
 // FilmLibrary. Populate the FilmLibrary using this method.
 // To conclude, print in the console the entire list of Films stored in the FilmLibrary, with all their fields.
+// 2. Add functionalities to the Film Library
+// add a set of methods to the FilmLibrary object, namely:
+// - sortByDate: returns a new array containing the Films within the FilmLibrary instance sorted in
+// ascending order of the watch date. The movies that the user has not already watched should be put at the
+// end. For example, after the sorting, the FilmLibrary shown in the previous exercise would look like:
+// ***** List of films *****
+// Id: 1, Title: Pulp Fiction, Favorite: true, Watch date: March 10, 2024, Score: 5, User: 1
+// Id: 2, Title: 21 Grams, Favorite: true, Watch date: March 17, 2024, Score: 4, User: 1
+// Id: 5, Title: Shrek, Favorite: false, Watch date: March 21, 2024, Score: 3, User: 1
+// Id: 3, Title: Star Wars, Favorite: false, Watch date: null, Score: 0, User: 1
+// Id: 4, Title: Matrix, Favorite: false, Watch date: null, Score: 0, User: 1
+// - deleteFilm: deletes a Film from the FilmLibrary based on an Id received by parameter.
+// - resetWatchedFilms: deletes the Watch date of all the Films in the FilmLibrary.
+// - getRated: selects the films that do have a defined score. Only movies with an assigned score should be
+// returned, ordered by decreasing score. After filtering the Films Library shown in exercise 1, the method
+// should print:
+// ***** Films filtered, only the rated ones *****
+// Id: 1, Title: Pulp Fiction, Favorite: true, Watch date: March 10, 2024, Score: 5
+// Id: 2, Title: 21 Grams, Favorite: true, Watch date: March 17, 2024, Score: 4
+// Id: 5, Title: Shrek, Favorite: false, Watch date: March 21, 2024, Score: 3
+// Hint: To implement the required functionalities described above you may use the functional programming
+// paradigm to manipulate the array of films.
+// Finally, test the methods by invoking them over the FilmLibrary instance you created and populated in
+// exercise 1.
+
+
 const dayjs = require('dayjs');
 
 function Film(id, title, isFavorite = false, dateWatched, rating, personId = 1) {
@@ -48,6 +74,30 @@ function FilmLibrary() {
     this.films = [];
     this.addNewFilm = function(film) {
         this.films.push(film);
+    };
+
+    this.sortByDate = function() {
+        return [...this.films].sort((a, b) => {
+            if (a.dateWatched === undefined) return 1;
+            if (b.dateWatched === undefined) return -1;
+            return a.dateWatched.diff(b.dateWatched);
+        });
+    };
+
+    this.deleteFilm = function(id) {
+        this.films = this.films.filter(film => film.id !== id);
+    };
+
+    this.resetWatchedFilms = function() {
+        this.films.forEach(film => {
+            film.dateWatched = undefined;
+        });
+    };
+
+    this.getRated = function() {
+        return [...this.films]
+            .filter(film => film.rating !== undefined)
+            .sort((a, b) => b.rating - a.rating);
     };
 }
 
